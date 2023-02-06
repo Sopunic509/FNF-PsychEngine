b94441200f39829575a408902f4af78f44215d92
3lPanaHaxeFLIXEL <121322637+Sopunic509@users.noreply.github.com>
2023-02-05 23:58:28 -0500

    Update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -134,10 +134,6 @@ class PauseSubState extends MusicBeatSubstate
 		regenMenu();
 		cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
 		
-		#if mobile
-		addVirtualPad(UP_DOWN, A);
-		addVirtualPadCamera();
-		#end
 	}
 
 	var holdTime:Float = 0;
